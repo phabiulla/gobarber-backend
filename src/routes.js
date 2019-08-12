d0ec071e9561f5eab3b1f3495f90809210df754f
phabiulla <phabiulla@gmail.com>
2019-08-12 11:10:39 +0100

    12.08: Correção de conflito de projetos

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,11 @@ import multer from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import MeetupController from './app/controllers/MeetupController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentsController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,8 +20,18 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
-routes.post('/meetups', MeetupController.store);
-routes.put('/meetups/:id', MeetupController.update);
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:id/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedules', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
